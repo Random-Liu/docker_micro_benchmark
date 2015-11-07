0ae8305a855606862e98014cbe9372406257012b
Lantao Liu <lantaol@google.com>
2015-11-06 18:33:06 -0800

    Add benchmark in benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-LC_ALL=C sar -rubwS -P ALL 1 > test.txt &
+MIN_DOCKER_NUMBER=6000
+
+DOCKER_NUMBER=`docker ps | wc -l`
+if [ $DOCKER_NUMBER -lt $MIN_DOCKER_NUMBER ]
+then
+  echo "Benchmark with different container number"
+  LC_ALL=C sar -rubwS -P ALL 1 > sar_benchmark_varies_containers.txt &
+  SAR_PID=$!
+  ./docker_micro_benchmark -c > latency_benchmark_varies_containers.txt
+  kill $SAR_PID
+fi
+
+echo "Benchmark with different period"
+LC_ALL=C sar -rubwS -P ALL 1 > sar_benchmark_varies_period.txt &
 SAR_PID=$!
-./docker_micro_benchmark
-#kill $SAR_PID 
+./docker_micro_benchmark -p > latency_benchmark_varies_period.txt
+kill $SAR_PID
